76f18cd71d601eaf99b0f2c75a47db1867770708
DeveloperLaravelPHP <150107310+DeveloperLaravelPHP@users.noreply.github.com>
2025-05-06 07:58:17 +0530

    remove hero section  commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,77 +116,7 @@ export default function Home() {
   }
   return (
     <>
-      <div className="bg-[rgb(42,108,101)] w-[100%] p-4 m-auto h-[850px] ">
-        <div className="flex flex-col sm:flex-row max-w-[1370px] w-full m-auto  ">
-          <div className="flex-[1.2] float-left lg:ml-16 md:ml-5 sm:ml-1">
-            <p className="leading-9 font-normal text-[25px] text-white font-Roboto mt-[50px]">
-              Your Trusted Partner in Stress-Free Bookkeeping
-            </p>
-
-            <span
-              className={`text-[#DAA520] border-b-[5px] text-left border-solid border-[#DAA520] rounded-[10px] capitalize -tracking-normal font-medium ${georgia.className}  text-[40px] sm:text-[40px] md:text-[55px] lg:text-[75px] leading-[55px] sm:leading-[55px] md:leading-[70px] lg:leading-[92px] `}
-            >
-              Focus on Growth,
-            </span>
-            <span
-              className={`text-white capitalize tracking-normal text-left outline-none font-medium ${georgia.className} text-[40px] sm:text-[40px] md:text-[55px] lg:text-[75px] leading-[55px] sm:leading-[55px] md:leading-[70px] lg:leading-[92px]  `}
-            >{`We'll Handle`}
-            </span>
-            <br />
-            <span
-              className={`text-white capitalize text-left font-medium ${georgia.className} text-[40px] sm:text-[40px] md:text-[55px] lg:text-[75px] leading-[55px] sm:leading-[55px] md:leading-[70px] lg:leading-[92px] `}
-            >
-              Your Books.{" "}
-            </span>
-            <p className="leading-6 font-normal text-[19px] text-white font-sans mt-[20px]">
-              Our certified bookkeepers provide seamless financial management,
-              accessible remotely from wherever you are, ensuring your books are
-              always accurate and up to date.
-            </p>
-            <p className="mt-[30px]">
-              <Link href="/">
-                <Button
-                  size="default"
-                  variant="default"
-                  className=" text-white font-Roboto font-[500] text-[20px] bg-[#daa521] rounded-[1px] pt-[9px] mt-[-8px] tb-[17px] px-[60px] h-[51px] transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-black hover:text-white  no-underline "
-                >
-                 
-                  Contact Us
-                </Button>
-              </Link>
-            </p>
-            <div className="flex flex-col md:flex-row w-full m-auto  ">
-              <div className="flex-1 mt-[30px] ">
-               
-                <Image
-                  src="/theme_images/Client.png"
-                  width={300}
-                  height={80}
-                  alt="home-banner"
-                  className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500"
-                ></Image>
-              </div>
-              <div className="flex-1 ">
-               
-                <p className="leading-8 mt-10 font-normal text-[23px] text-white font-Roboto">
-                  Trusted over 250+ client
-                  <br /> accorss country
-                </p>{" "}
-              </div>
-            </div>
-          </div>
-
-          <div className="flex-1 hidden md:block">
-            <Image
-              src="/theme_images/home-banner.png"
-              width={523}
-              height={750}
-              alt="home-banner"
-              className="transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:backdrop-blur-md"
-            ></Image>
-          </div>
-        </div>
-      </div>
+     
 
       {section2 !== null && section2.status === "true" && (
         <>
